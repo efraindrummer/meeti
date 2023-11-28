955812bbf8b84994b0a4b2cf2a619fb3aa1786ca
Efrain May <efrain_may_75@hotmail.com>
2023-11-28 14:43:00 -0600

    meeti init - connection db postgres, nad desing layout ejs

---
+++ index.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const path = require('path');
+const router = require('./routes');
+const expressEjsLayouts = require('express-ejs-layouts');
+require('dotenv').config({path: 'variables.env'});
+
+const db = require('./config/db');
+db.sync().then(
+    () => console.log('DB CONECTADA')
+).catch(
+    (error) => console.log(error)
+);
+
+const app = express();
+
+//habilitar ejs como template engine
+app.use(expressEjsLayouts);
+app.set('view engine', 'ejs');
+
+//ubicacion de vistas
+app.set('views', path.join(__dirname, './views'));
+
+//archivos estaticos
+app.use(express.static('public'));
+
+//middleware(usuario logueado, flash messages, fecha actual);
+app.use((req, res, next) => {
+    const fecha = new Date();
+    res.locals.year = fecha.getFullYear();
+    next();
+});
+
+//routing
+app.use('/', router());
+//puerto
+app.listen(process.env.PORT, () => {
+    console.log('EL servidor esta funcionando');
+});

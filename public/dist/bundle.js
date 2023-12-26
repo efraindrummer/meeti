eab3fc4623dc2c2a05ad9c537ebc2f0e2d3b0ebb
efracode <efrain_may_75@hotmail.com>
2023-12-25 21:00:12 -0600

    maps

--- public/dist/bundle.js
+++ public/dist/bundle.js
@@ -15,7 +15,7 @@
   \**************************/
 /***/ (() => {
 
-eval("\n\n//# sourceURL=webpack://meetup/./public/js/app.js?");
+eval("/* var map = L.map('mapa').setView([51.505, -0.09], 13);\r\n\r\nL.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {\r\n    attribution: '&copy; <a href=\"https://www.openstreetmap.org/copyright\">OpenStreetMap</a> contributors'\r\n}).addTo(map);\r\n\r\nL.marker([51.5, -0.09]).addTo(map).bindPopup('A pretty CSS popup.<br> Easily customizable.').openPopup(); */\n\ndocument.addEventListener('DOMContentLoaded', function () {\n  var contenedorMapa = document.querySelector('.contenedor-mapa');\n  if (contenedorMapa) {\n    var map = L.map('map').setView([51.505, -0.09], 13);\n  }\n});\n\n//# sourceURL=webpack://meetup/./public/js/app.js?");
 
 /***/ })
 
